47dfdbe500ef3fcb7e4bc9c437facc0c85219541
Abdelhalim Serhani <84025063+serhabdel@users.noreply.github.com>
2024-10-09 20:55:09 +0000

    aneww

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Optional: Smooth Scrolling for Navigation Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
